4f9e6f0c1889f4cd8c831d1a7cdf1cebae318b0a
timazubar <timazubar33@gmail.com>
2020-08-06 17:46:23 +0300

    feat: make all buttons work according to the task

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, SimpleChanges } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { interval, Subscription, Subject, NEVER } from 'rxjs';
+import { map, scan, startWith, tap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stopwatch',
@@ -11,41 +11,58 @@ export class StopwatchComponent implements OnInit {
   counter$ = interval(1000);
   sub: Subscription;
 
+  running = false;
+
   hours = 0;
   minutes = 0;
   seconds;
 
+  private counterSubject: Subject<{
+    pause: boolean;
+    counterValue?: number;
+  }> = new Subject();
+
   constructor() {}
 
-  start() {
-    if (!this.sub) {
-      this.sub = this.counter$
-        .pipe(
-          map((value) => (value < 10 ? '0' + value : value)),
-          map((seconds) => (seconds > 60 ? this.minutes++ : seconds))
+  private initializeCounter() {
+    this.counterSubject
+      .pipe(
+        startWith({ pause: true, counterValue: 0 }),
+        scan((acc, val) => ({ ...acc, ...val })),
+        tap((state) => {
+          this.seconds = state.counterValue;
+        }),
+        switchMap((state) =>
+          state.pause
+            ? NEVER
+            : interval(1000).pipe(
+                tap((val) => {
+                  state.counterValue += 1;
+                  this.seconds = state.counterValue;
+                })
+              )
         )
-        .subscribe((sec) => (this.seconds = sec));
-      console.log(this.sub);
-    } else {
-      this.sub.closed = false;
-    }
+      )
+      .subscribe();
+  }
+
+  start() {
+    this.counterSubject.next({ pause: false });
   }
 
   stop() {
-    if (this.sub) {
-      console.log(this.sub);
-      this.sub.unsubscribe();
-    }
+    this.counterSubject.next({ pause: true, counterValue: 0 });
   }
 
   reset() {
-    this.stop();
-    this.start();
+    this.counterSubject.next({ pause: false, counterValue: 0 });
   }
 
   wait() {
-    this.stop();
+    this.counterSubject.next({ pause: true });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeCounter();
+  }
 }
