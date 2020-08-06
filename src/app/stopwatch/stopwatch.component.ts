57a60e3db136e37f2b030adebe6b3e8aa2ac337c
timazubar <timazubar33@gmail.com>
2020-08-06 16:57:08 +0300

    feat: add basic methods for buttons

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stopwatch',
@@ -6,7 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stopwatch.component.scss'],
 })
 export class StopwatchComponent implements OnInit {
+  counter$ = interval(1000);
+  sub: Subscription;
+
+  hours = 0;
+  minutes = 0;
+  seconds;
+
   constructor() {}
+
+  start() {
+    if (!this.sub) {
+      this.sub = this.counter$
+        .pipe(
+          map((value) => (value < 10 ? '0' + value : value)),
+          map((seconds) => (seconds > 60 ? this.minutes++ : seconds))
+        )
+        .subscribe((sec) => (this.seconds = sec));
+      console.log(this.sub);
+    } else {
+      this.sub.closed = false;
+    }
+  }
+
+  stop() {
+    if (this.sub) {
+      console.log(this.sub);
+      this.sub.unsubscribe();
+    }
+  }
+
+  reset() {
+    this.stop();
+    this.start();
+  }
+
+  wait() {
+    this.stop();
+  }
 
   ngOnInit(): void {}
 }
